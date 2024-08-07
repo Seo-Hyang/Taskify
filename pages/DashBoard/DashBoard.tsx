e91126b24369477b4b3d5b503a753134f0c2fdbe
Seo-Hyang <hyangaroma@gmail.com>
2024-08-07 23:41:21 +0900

    사이드메뉴 데이터 연동

--- pages/DashBoard/DashBoard.tsx
+++ pages/DashBoard/DashBoard.tsx
@@ -1,16 +1,24 @@
+// 기본 import
 import styles from "@/pages/DashBoard/style.module.scss";
 import Header from "@/components/Header/Header";
 import SideMenu from "@/components/SideMenu/SideMenu";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+// 컴포넌트 import
 import DashboardColumn from "@/components/DashboardColumns/DashboardColumns";
 import AddButton from "@/components/Button/AddButton/AddButton";
-import Head from "next/head";
 
 export default function DashBoard() {
+  //대시보드 id에 따라 페이지 이동
+  const router = useRouter();
+  const { dashboardid } = router.query;
+
   return (
     <>
       <div>
         <Head>
-          <title>Taskify-대시보드</title>
+          <title>Taskify-대시보드 {dashboardid}</title>
         </Head>
       </div>
       <section>
