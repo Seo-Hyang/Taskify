7d8dc209188a32c1c716c801f0667800ae4b300a
Seo-Hyang <hyangaroma@gmail.com>
2024-08-09 18:37:59 +0900

    내 대시보드 목록 DashboardListButton 컴포넌트 작업

--- pages/dashboards/index.tsx
+++ pages/dashboards/index.tsx
@@ -1,6 +1,28 @@
 //기본 import
+// import Header from "@/components/Header/Header";
+// import SideMenu from "@/components/SideMenu/SideMenu";
+import Head from "next/head";
 
-import Header from "@/components/Header/Header";
-import SideMenu from "@/components/SideMenu/SideMenu";
+//컴포넌트 import
+import DashboardListButton from "@/components/Button/DashboardListButton/DashboardListButton";
 
-export default function DashBoards() {}
+export default function DashBoards() {
+  return (
+    <div>
+      <section>
+        <Head>
+          <title>Taskify-대시보드</title>
+        </Head>
+        {/* <section>
+          <SideMenu />
+          <Header>내 대시보드</Header>
+        </section> */}
+      </section>
+      <section>
+        <DashboardListButton color="#000000" isOwn={true}>
+          asd
+        </DashboardListButton>
+      </section>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <Link href="/SignUpPage">▷회원가입</Link>
         </li>
         <li>
-          <Link href="/dashboards/11370">▷대시보드</Link>
+          <Link href="/dashboards">▷대시보드</Link>
         </li>
         <li>
           <Link href="/MyPage/MyPage">▷마이 페이지</Link>
