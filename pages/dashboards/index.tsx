24238ce17a0618256274d48c533248751a76f9ff
Seo-Hyang <hyangaroma@gmail.com>
2024-08-10 02:16:14 +0900

    대시보드 초대받은 목록 데이터 연동

--- pages/dashboards/index.tsx
+++ pages/dashboards/index.tsx
@@ -4,13 +4,14 @@ import SideMenu from "@/components/SideMenu/SideMenu";
 import Head from "next/head";
 import styles from "@/pages/dashboards/style.module.scss";
 import { useEffect, useState } from "react";
-import { Dashboard } from "@/types/dashboard";
+import { Dashboard, DashboardInvitation } from "@/types/dashboard";
 import instance from "@/lib/axios";
 
 //컴포넌트 import
 import DashboardListButton from "@/components/Button/DashboardListButton/DashboardListButton";
 import AddButton from "@/components/Button/AddButton/AddButton";
 import PageButton from "@/components/Button/PageButton/PageButton";
+import ArrowButton from "@/components/Button/ArrowButton/ArrowButton";
 
 /**
  * To do
@@ -25,19 +26,37 @@ import PageButton from "@/components/Button/PageButton/PageButton";
 
 export default function DashBoards() {
   const [dashboardList, setDashboardList] = useState<Dashboard[]>([]); //대시모드 목록
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1); //페이지 이동에 따라 변경
+  const [invitedList, setInvitedList] = useState<DashboardInvitation[]>([]); //초대받은 대시보드 목록
+
+  const pageSize = 6;
 
   async function getDashboardList() {
     const res = await instance.get(
-      "/dashboards?navigationMethod=pagination&page=1&size=6"
+      `/dashboards?navigationMethod=pagination&page=1&size=${pageSize}`
     );
     const nextDashboardList = res.data;
     const { dashboards, totalCount, cursorId } = nextDashboardList;
 
+    const pageCnt = Math.trunc(totalCount / pageSize) + 1;
+
     setDashboardList(dashboards);
+    setPageCount(pageCnt);
   }
 
+  async function getInvitedList() {
+    const res = await instance.get(`/invitations?size=${pageSize}`);
+    const nextDashboardList = res.data;
+    const { invitations, cursorId } = nextDashboardList;
+
+    setInvitedList(invitations);
+  }
+
+  //대시보드 목록, 페이지 수, 현재 페이지
   useEffect(() => {
     getDashboardList();
+    getInvitedList();
   }, []);
 
   return (
@@ -67,9 +86,12 @@ export default function DashBoards() {
               ))}
             </section>
             <div className={styles.dashboard_pageCursor}>
-              페이지 수, 화살표 버튼
+              {pageCount} 페이지 중 {currentPage}
+              <div>
+                <ArrowButton leftArrow={true} />
+                <ArrowButton rightArrow={true} />
+              </div>
             </div>
-            <hr></hr>
           </section>
           <section className={styles.dashboard_invitedList}>
             <section className={styles.dashboard_invitedTitle}>
@@ -82,22 +104,23 @@ export default function DashBoards() {
                 <p className={styles.column_inviter}>초대자</p>
                 <p className={styles.column_button}>수락여부</p>
               </div>
-              <section className={styles.dashboard_invitedContainer}>
-                <div className={styles.invited_title}>대시보드 타이틀</div>
-                <div className={styles.invited_inviter}>초대자</div>
-                <div className={styles.dashboard_invitedButtons}>
-                  <PageButton>수락</PageButton>
-                  <PageButton isCancled={true}>거절</PageButton>
-                </div>
-              </section>
-              <section className={styles.dashboard_invitedContainer}>
-                <div className={styles.invited_title}>대시보드 타이틀</div>
-                <div className={styles.invited_inviter}>초대자</div>
-                <div className={styles.dashboard_invitedButtons}>
-                  <PageButton>수락</PageButton>
-                  <PageButton isCancled={true}>거절</PageButton>
-                </div>
-              </section>
+              {invitedList.map((item) => (
+                <section
+                  key={item.id}
+                  className={styles.dashboard_invitedContainer}
+                >
+                  <div className={styles.invited_title}>
+                    {item.dashboard.title}
+                  </div>
+                  <div className={styles.invited_inviter}>
+                    {item.inviter.nickname}
+                  </div>
+                  <div className={styles.dashboard_invitedButtons}>
+                    <PageButton>수락</PageButton>
+                    <PageButton isCancled={true}>거절</PageButton>
+                  </div>
+                </section>
+              ))}
             </section>
           </section>
         </section>
