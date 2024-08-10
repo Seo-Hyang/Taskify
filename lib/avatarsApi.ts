7827a83b7e87541028dbc956648ee113df474856
ohjohwan <whghks1020@naver.com>
2024-08-10 18:19:16 +0900

    modify profile avatar background color code

--- lib/avatarsApi.ts
+++ lib/avatarsApi.ts
@@ -5,12 +5,31 @@ function getRandomPastelColor() {
   const red = randomValue();
   const green = randomValue();
   const blue = randomValue();
-  return `${red.toString(16).padStart(2, '0')}${green.toString(16).padStart(2, '0')}${blue.toString(16).padStart(2, '0')}`;
+  return `${red.toString(16).padStart(2, "0")}${green
+    .toString(16)
+    .padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`;
+}
+
+// 로컬 스토리지에서 색상 가져오기 및 생성, 저장 함수
+function getCreateProfileColor(email: string) {
+  if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
+    const colorKey = `profileColor-$(email)`;
+    let color = localStorage.getItem(colorKey);
+
+    if (!color) {
+      color = getRandomPastelColor();
+      localStorage.setItem(colorKey, color);
+    }
+
+    return color;
+  } else {
+    return getRandomPastelColor();
+  }
 }
 
 // 프로필 이미지 URL을 생성하는 함수
 export function generateProfileImageUrl(email: string) {
   const initials = email.charAt(0).toUpperCase();
-  const backgroundColor = getRandomPastelColor();
+  const backgroundColor = getCreateProfileColor(email);
   return `https://ui-avatars.com/api/?name=${initials}&background=${backgroundColor}&color=000&rounded=true`;
 }
