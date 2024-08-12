0435235e46c0ea92319eaade8c95588706889cd1
Seo-Hyang <hyangaroma@gmail.com>
2024-08-12 22:58:40 +0900

    landing 페이지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
 
   return (
     <>
-      {/* <LandingPage /> - / 페이지 테스트 */}
+      <LandingPage />
+      {/* <LandingPage /> - / 페이지 테스트
       <ul>
         <li>
           <Link href="/LandingPage">▷랜딩 페이지</Link>
@@ -40,7 +41,7 @@ export default function Home() {
         <li>
           <button onClick={handleLogout}>로그아웃</button>
         </li>
-      </ul>
+      </ul> */}
     </>
   );
 }
